671287bf7577404556e6a01a701d11054d346fe3
cimendes <iines.mendes@gmail.com>
2019-08-14 15:06:30 +0100

    add nextflow accessory scripts (help and param check)

---
+++ lib/Helper.groovy
@@ -0,0 +1,92 @@
+class Help {
+
+    static def start_info(Map info, String time, String profile) {
+
+        println ""
+        println "============================================================"
+        println "                    NastyMetagenomes"
+        println "============================================================"
+        println ""
+        println ""
+        if (info.containsKey("fastq")){
+        int nsamples = info.fastq / 2
+        println " Input FastQ                 : $info.fastq"
+        println " Input samples               : $nsamples"
+        }
+        if (info.containsKey("fasta")){
+        println " Input Fasta                 : $info.fasta"
+        }
+        if (info.containsKey("accessions")){
+        println " Input accessions            : $info.accessions"
+        }
+        println " Reports are found in        : ./reports"
+        println " Results are found in        : ./results"
+        println " Profile                     : $profile"
+        println ""
+        println "Starting pipeline at $time"
+        println ""
+
+    }
+
+    static void complete_info(nextflow.script.WorkflowMetadata wf) {
+
+        println ""
+        println "Pipeline execution summary"
+        println "=========================="
+        println "Completed at                 : $wf.complete"
+        println "Duration                     : $wf.duration"
+        println "Success                      : $wf.success"
+        println "Work directory               : $wf.workDir"
+        println "Exit status                  : $wf.exitStatus"
+        println ""
+
+    }
+
+    // TODO - Add parameters
+    static def print_help(Map params) {
+
+        println ""
+        println "============================================================"
+        println "                      NastyMetagenomes"
+        println "============================================================"
+        println ""
+        println ""
+        println ""
+        println "Usage: "
+        println "    nextflow run main.nf"
+        println ""
+        println "       --accessions                Path file with accessions, one perline. (default: $params.fastq) (default: null)"
+        println ""
+        println "       --fastq                     Path expression to paired-end fastq files. (default: $params.fastq) (default: 'fastq/*_{1,2}.*')"
+
+    }
+
+}xxxxx
+
+class CollectInitialMetadata {
+
+    public static void print_metadata(nextflow.script.WorkflowMetadata workflow){
+
+        def treeDag = new File("${workflow.projectDir}/.treeDag.json").text
+        def forkTree = new File("${workflow.projectDir}/.forkTree.json").text
+
+        def metadataJson = "{'nfMetadata':{'scriptId':'${workflow.scriptId}',\
+'scriptName':'${workflow.scriptName}',\
+'profile':'${workflow.profile}',\
+'container':'${workflow.container}',\
+'containerEngine':'${workflow.containerEngine}',\
+'commandLine':'${workflow.commandLine}',\
+'runName':'${workflow.runName}',\
+'sessionId':'${workflow.sessionId}',\
+'projectDir':'${workflow.projectDir}',\
+'launchDir':'${workflow.launchDir}',\
+'startTime':'${workflow.start}',\
+'dag':${treeDag},\
+'forks':${forkTree}}}"
+
+        def json = metadataJson.replaceAll("'", '"')
+
+        def jsonFile = new File(".metadata.json")
+        jsonFile.write json
+    }
+}
---
+++ main.nf
@@ -0,0 +1,17 @@
+#!/usr/bin/env nextflow
+
+import Helper
+import CollectInitialMetadata
+
+// Pipeline version
+if (workflow.commitId){
+    version = "2.2 $workflow.revision"
+} else {
+    version = "2.2 (local version)"
+}
+
+params.help = false
+if (params.help){
+    Help.print_help(params)
+    exit 0
+}
