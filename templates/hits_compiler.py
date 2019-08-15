48517cb7b58a30fdd981d9e8f8349e9d1d1d8199
jlparki <jlparkinson1@hackathon2.us-east1-b.c.strides-microbial-hackathon.internal>
2019-08-15 09:40:34 -0400

    added updated hmm pipeline

---
+++ templates/hits_compiler.py
@@ -0,0 +1,44 @@
+import Bio, os
+from Bio import SeqIO
+
+
+current_dir = os.getcwd()
+os.chdir('hmm_results_temp')
+file_list = [x for x in os.listdir() if x.startswith('hmmresult')]
+hit_list = []
+#Loop through all files in the temporary hmm_results_temp directory.
+#Parse each file, looking to see whether it contains any results listed
+#under the '------' line. If so, add the accession number stored early
+#in the file to a list of hits.
+for inp_file in file_list:
+    file_handle = open(inp_file)
+    collect_outputs = False
+    for line in file_handle:
+        if 'Accession:' in line:
+            accession_id = line.strip().split()[1]
+        if collect_outputs == True:
+            if len(line) > 1:
+                if '[No hits detected that satisfy' in line or 'Domain annotation' in line:
+                    break
+                else:
+                    if '_forward_' in line or '_reverse_' in line:
+                        hit_list.append(accession_id)
+                        break
+        if '------' in line:
+            collect_outputs = True
+    file_handle.close()
+
+#Now go to the nf_to_seq.fa file, which contains a reprentative nucleotide
+#sequence for each NF accession number. For each accession number that had a
+#hit, retrieve the appropriate nucleotide sequence and write it to
+#hit_nucleotide_seqs.fa, which will be our output to the next stage of
+#the pipeline.
+hit_set = set(hit_list)
+os.chdir(current_dir)
+hit_nucleotide_seqs = []
+with open('hit_nucleotide_seqs.fa', 'w+') as hit_file:
+    with open('nf_to_seq.fa') as nf_map_file:
+        for record in SeqIO.parse(nf_map_file, 'fasta'):
+            if record.id.split('|')[0] in hit_set:
+                hit_nucleotide_seqs.append(record)
+        SeqIO.write(hit_nucleotide_seqs, hit_file, 'fasta')
--- templates/hmm_pipeline.sh
+++ templates/hmm_pipeline.sh
@@ -3,24 +3,47 @@ set -e
 set -u
 set -o pipefail
 
-while getopts ':q::h:' OPTION; do
+while getopts ':q::h::a:' OPTION; do
        case "$OPTION" in
 	       q)    queryfile="$OPTARG";;
+	       a)    threshold="$OPTARG";;
 	       h)    hmmdir="$OPTARG";;
-               ?)    echo "Script usage: [-h] [dir containing hmm profiles] [-q] [gzipped fastq file]"
+               ?)    echo "Script usage: [-h] [dir containing hmm profiles] [-q] [fastq file] [-a] [threshold bitscore for hmmer]"
        esac
-done       
+done      
 
+if (( $OPTIND == 1)); then
+	echo "Incorrect arguments supplied."
+	echo "To run hmm_pipeline.sh, supply the following arguments:"
+	echo "[-h] [directory containing hmm profiles] [-q] [fastq file]"
+	echo "[-a] [bitscore threshold for read to be considered a hit]"
+	echo "in any order."
+	exit 1
+fi
+
+#Start by translating each read in all 6 possible reading frames using
+#the read_translator.py script.
 count=0
 python read_translator.py $queryfile -testmode
+echo "reads translated; beginning HMMsearch; time for each profile will be printed"
+#Use hmmsearch to bounce all 562 HMMs against the fasta file containing the translated reads
 for hmmfile in $hmmdir/*.HMM;
 do
-	time hmmsearch --noali -T 50 --acc --incT 50 -o "hmmresult${count}" $hmmfile translated_reads.fa
+	hmmsearch --noali -T $threshold --acc --incT $threshold -o "hmmresult${count}" $hmmfile translated_reads.fa
 	count=`expr $count + 1`
 done
 
+echo "hmm searches complete"
+#Move all the results files to a temporary directory we will delete later.
 mkdir hmm_results_temp
 mv hmmresult* hmm_results_temp/
 
-python do_things_to_hmm_output #add this next
-#after that add local assembly.... then delete all the temporary hmmresult files.
+#Use the hits_compiler script to retrieve a representative nucleotide sequence
+#corresponding to each hit from the nf_to_seq fasta file and output to
+#hit_nucleotide_seqs.fa, which will be the target for the guided assembler.
+python hits_compiler.py $queryfile
+
+#Remove all the temporary files
+rm hit_reads.fa translated_reads.fa
+rm -r hmm_results_temp
+echo "HMM analysis complete; moving to guided assembly."
